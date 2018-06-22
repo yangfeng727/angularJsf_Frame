fd8e1a4c75f97e4c1a9efb3ed9a4b87323330b41
yangfeng727 <896148932@qq.com>
2018-06-22 16:30:04 +0800

    yangfeng commit

--- dev/js/controller/page2.js
+++ dev/js/controller/page2.js
@@ -11,15 +11,22 @@
 
         $('.page').css('background','blue');
 
+        var variable='';// 我是变量
+        var idD='page2Echarts';
+        $('#'+idD).html('');
         // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.getElementById('page2Echarts'));
-
+        var myChart = echarts.init(document.getElementById(idD));
         // 指定图表的配置项和数据
         var option = {
             title: {
                 text: 'ECharts 入门示例'
             },
-            tooltip: {},
+            tooltip: {
+                trigger:'axis',// 注意整个得设置为axis
+                formatter:function (param) {
+                    variable=param[0].name;
+                }
+            },
             legend: {
                 data:['销量']
             },
@@ -30,12 +37,16 @@
             series: [{
                 name: '销量',
                 type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
+                data: [0, 20, 36, 10, 10, 20]
             }]
         };
-
-
         myChart.setOption(option);
+        $('#'+idD).off('click').on('click',function () {
+            console.log(variable)
+        });
+        // myChart.on('click',function(param){
+        //     console.log(param.name)
+        // });
 
         // echarts tooltip 轮播
         window.GLOBAL.toolTipLunBoAll(myChart);
